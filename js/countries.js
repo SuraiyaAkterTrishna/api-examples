a479bae0a79de71ec2070d7aa80539efde2c42cf
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-07 15:09:42 +0600

    country api done

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,23 @@
-console.log('countries connected');
+const loadCountries = () => {
+    const url = 'https://restcountries.com/v3.1/all';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+};
+const displayCountries = (countries) => {
+    /* for (const country of countries) {
+        console.log(country);
+    } */
+    const countriesContainer = document.getElementById('countries-container');
+    countries.forEach(country => {
+        console.log(country);
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+            <h3>Name: ${country.name.common}</h3>
+            <p>Capital: ${country.capital ? country.capital[0] : 'No capital'}</p>
+        `;
+        countriesContainer.appendChild(countryDiv);
+    })
+}
+loadCountries();
