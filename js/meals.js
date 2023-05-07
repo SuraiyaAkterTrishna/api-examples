022ed586e4156e15c1cc4330f8e203e93312c342
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-07 21:42:37 +0600

    meal db done

--- js/meals.js
+++ js/meals.js
@@ -1,24 +1,54 @@
-const loadMeals = () => {
-  url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a";
+const loadMeals = (search) => {
+  url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayMeals(data.meals));
 };
 const displayMeals = (meals) => {
   const mealsContainer = document.getElementById("meals-container");
+  mealsContainer.innerHTML = ``;
   meals.forEach((meal) => {
+    // console.log(meal);
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-        <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <div onclick="loadMealDetails(${meal.idMeal})" class="card">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
         </div>
         </div>
         `;
     mealsContainer.appendChild(mealDiv);
   });
 };
-loadMeals();
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadMeals(searchText);
+  searchField.value = ``;
+};
+const loadMealDetails = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+const displayMealDetails = (meal) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = ``;
+  const detailDiv = document.createElement("div");
+  detailDiv.classList.add("card");
+  detailDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    detailContainer.appendChild(detailDiv);
+};
+
+loadMeals("");
