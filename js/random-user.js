8f5247ca1287c35430350838af3f2512a55018b6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-07 13:32:46 +0600

    random user done

--- js/random-user.js
+++ js/random-user.js
@@ -1,1 +1,22 @@
-console.log('Random user connected');
+const loadUsers = () => {
+    url = 'https://randomuser.me/api/?results=10';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayUsers(data.results));
+};
+const displayUsers = users => {
+    const userContainer = document.getElementById('users-container');
+    for (const user of users) {
+        console.log(user);
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('user');
+    userDiv.innerHTML = `
+        <h3>Name: ${user.name.title} ${user.name.first} ${user.name.last}</h3>
+        <p>Email: ${user.email}</p>
+        <p>Location: ${user.location.city} ${user.location.country}</p>
+    `;
+    userContainer.appendChild(userDiv);
+    };
+
+}
+loadUsers();
